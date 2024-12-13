267b43bcc0ab7dcadd7b9124bea50ca08d88c98b
VTIOS <thanhhaiten1@gmail.com>
2024-12-13 16:06:43 +0700

    custom-theme.lua

--- custom-theme.lua
+++ custom-theme.lua
@@ -1,8 +1,8 @@
 return {
     ['Yae'] = {
-        ['Background'] = 'rbxassetid://18675204938',
-        ['Character'] = 'rbxassetid://18388637422',
-        ['Icon'] = 'rbxassetid://18675236797',
+        ['Background'] = 'rbxassetid://7229442422',
+        ['Character'] = 'rbxassetid://7229442422',
+        ['Icon'] = 'rbxassetid://7229442422',
         ['Button Image'] = 'rbxassetid://18675335801',
         ['Color'] = {
               ['Text'] = Color3.fromRGB(221, 86, 92),
