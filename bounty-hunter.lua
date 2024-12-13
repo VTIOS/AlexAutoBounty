92463fe26c2184ed2baed2cd478e24cabe9773fa
VTIOS <thanhhaiten1@gmail.com>
2024-12-13 15:54:53 +0700

    Add files via upload

---
+++ bounty-hunter.lua
@@ -0,0 +1,1 @@
+loadstring(game:HttpGet('https://raw.githubusercontent.com/ErutTheTeru/ruahub/refs/heads/main/bounty-update.lua'))()
---
+++ custom-theme.lua
@@ -0,0 +1,12 @@
+return {
+    ['Yae'] = {
+        ['Background'] = 'rbxassetid://18675204938',
+        ['Character'] = 'rbxassetid://18388637422',
+        ['Icon'] = 'rbxassetid://18675236797',
+        ['Button Image'] = 'rbxassetid://18675335801',
+        ['Color'] = {
+              ['Text'] = Color3.fromRGB(221, 86, 92),
+              ['Stroke'] = Color3.fromRGB(104, 47, 46)
+        }
+    }
+}
